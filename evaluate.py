176d093c0c99871fe10c3a409f02f0ebb480d5e1
guixiangyu <guixiangyu1@gmail.com>
2018-09-04 13:33:57 +0800

    init

--- evaluate.py
+++ evaluate.py
@@ -73,7 +73,7 @@ def main():
 
     # build model
     model = NERModel(config)
-    model.build("fine_tuning")
+    model.build("train")
     model.restore_session(config.dir_model)
 
     # create dataset
