356d9f798b6272b55413bc177065133a516f8c53
guixiangyu <guixiangyu1@gmail.com>
2018-09-05 17:12:20 +0800

    init

--- model/data_utils.py
+++ model/data_utils.py
@@ -415,6 +415,7 @@ def export_trimmed_glove_vectors(vocab, glove_filename, trimmed_filename, dim):
                     word_idx = vocab["$UNK$"]
                 embedding_total.append(embeddings[word_idx])
                 embeddings[keyword_index] = np.mean(embedding_total, axis=0)
+                print(embeddings[keyword_index])
 
     np.savez_compressed(trimmed_filename, embeddings=embeddings)    #压缩词嵌入到文件中，并且名字为embeddings
 
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from model.data_utils import CoNLLDataset1, get_vocabs, get_processing_word
 from model.config import Config
+import numpy as np
 
 
 if __name__ == '__main__':
     config = Config()
-    with open(config.filename_words) as f:
-        for line in f:
-            if "Peter$@&Blackburn" in line:
-                print(True)
+    embeddings = np.load(config.filename_trimmed)
+    for i in embeddings:
+        print(i)
 
 
 
