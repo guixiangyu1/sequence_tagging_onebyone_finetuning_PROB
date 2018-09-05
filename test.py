a0f9db713dbbf76da60dd22ebb9cc12a4abcc38b
guixiangyu <guixiangyu1@gmail.com>
2018-09-05 16:02:46 +0800

    init

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
-from model.data_utils import CoNLLDataset, get_vocabs, get_processing_word
+from model.data_utils import CoNLLDataset1, get_vocabs, get_processing_word
 from model.config import Config
 
 
 if __name__ == '__main__':
     config = Config()
-    i, j = 0, 0
-    with open(config.filename_train) as f:
+    with open(config.filename_words) as f:
         for line in f:
-            if "$NUM$" in line:
-                i += 1
-            if "$UNK$" in line:
-
-                j += 1
-        print(i, j)
+            if "Peter$@&Blackburn" in line:
+                print(True)
 
 
 
