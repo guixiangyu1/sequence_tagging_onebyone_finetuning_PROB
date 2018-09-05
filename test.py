4122d12ee0a094912618a47f28c9635f998a3893
guixiangyu <guixiangyu1@gmail.com>
2018-09-05 15:44:14 +0800

    init

--- test.py
+++ test.py
@@ -4,13 +4,15 @@ from model.config import Config
 
 if __name__ == '__main__':
     config = Config()
-
+    i, j = 0, 0
     with open(config.filename_test) as f:
         for line in f:
             if "$NUM$" in line:
-                print(1)
+                i += 1
             if "$UNK$" in line:
-                print(2)
+
+                j += 1
+        print(i, j)
 
 
 
