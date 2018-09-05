3ce84b1faeea975a6a05c069ae9288b8434eae6b
guixiangyu <guixiangyu1@gmail.com>
2018-09-05 15:40:02 +0800

    init

--- test.py
+++ test.py
@@ -4,19 +4,13 @@ from model.config import Config
 
 if __name__ == '__main__':
     config = Config()
-    dev = CoNLLDataset(config.filename_dev, config.processing_word,
-                       config.processing_tag, config.max_iter)
-    i = 0
-    for words,tags,masks in dev:
-        pass
-    print(1)
-    for words,tags,masks in dev:
-        pass
-    print(2)
-    for words,tags, masks in dev:
-        if i < 20:
-            print(words)
-        i+=1
+
+    with open(config.filename_glove) as f:
+        for line in f:
+            if "$NUM$" in line:
+                print(1)
+            if "$UNK$" in line:
+                print(2)
 
 
 
