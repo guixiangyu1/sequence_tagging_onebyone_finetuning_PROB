4970c5766bcb2ff891b806d5ec1b06d042255651
guixiangyu <guixiangyu1@gmail.com>
2018-09-06 22:44:17 +0800

    init

--- train_fine_tuning.py
+++ train_fine_tuning.py
@@ -9,7 +9,7 @@ def main():
     config = Config()
     config.nepochs          = 200
     config.dropout          = 0.5
-    config.batch_size       = 40
+    config.batch_size       = 60
     config.lr_method        = "adam"
     config.lr               = 0.0005
     config.lr_decay         = 1.0
