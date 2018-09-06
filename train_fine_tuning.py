480733de39741034acd774f806c62231b39dd560
guixiangyu <guixiangyu1@gmail.com>
2018-09-06 20:28:08 +0800

    init

--- train_fine_tuning.py
+++ train_fine_tuning.py
@@ -9,9 +9,9 @@ def main():
     config = Config()
     config.nepochs          = 200
     config.dropout          = 0.5
-    config.batch_size       = 10
+    config.batch_size       = 40
     config.lr_method        = "adam"
-    config.lr               = 0.0001
+    config.lr               = 0.0005
     config.lr_decay         = 1.0
     config.clip             = -5.0 # if negative, no clipping
     config.nepoch_no_imprv  = 10
